dfbec364ddda6c81b8219c183c95118f036522b2
Squire Johnson <squire.a.johnson@gmail.com>
2015-08-26 17:02:17 -0600

    routes set up

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,15 @@
+(function(){
 'use strict';
 
-angular
-  .module('prepTrack', []);
+var app = angular.module('prepTrack', ['ui.router']);
+	app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.when('', '/');
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('main', {
+          url: '/',
+          templateUrl : 'app/main/mainView.html',
+          controller  : 'MainCtrl'
+      });
+	});
+})();
---
+++ public/app/main/mainCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('prepTrack')
+  .controller('MainCtrl', function ($scope) { 
+
+
+  });
